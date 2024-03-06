d7ad655b90641fd95f1bf3539fb26ee74b25078a
takahiro.hashimoto <bridge.takahiro.1539@gmail.com>
2024-03-06 10:39:37 +0000

    set timezon

--- config/app.php
+++ config/app.php
@@ -68,8 +68,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Tokyo',
 
     /*
     |--------------------------------------------------------------------------
